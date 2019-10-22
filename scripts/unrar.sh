787bb91fa18aa964320049b22329f3d7c77c89ba
vikekh <621212+vikekh@users.noreply.github.com>
2019-10-22 19:20:25 +0200

    Debug log

--- scripts/unrar.sh
+++ scripts/unrar.sh
@@ -3,9 +3,11 @@
 cd $TR_TORRENT_DIR
 
 if [ ! -d $TR_TORRENT_NAME ] ; then
+    echo "Unable to find torrent directory, exiting early."
     exit 0
 fi
 
+echo "Begin unrar"
 cd $TR_TORRENT_NAME
 UNRAR_DIR=./unrar
 mkdir $UNRAR_DIR
@@ -15,3 +17,4 @@ find . -name *.rar -exec unrar e -o- {} \;
 find . -name *.rar -exec rm {} \;
 cd ..
 rmdir -r $UNRAR_DIR
+echo "End unrar"
