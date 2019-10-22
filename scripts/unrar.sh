a032b5fefb7a149510cfae1fd50046485b8a5c8b
vikekh <621212+vikekh@users.noreply.github.com>
2019-10-22 19:02:25 +0200

    Update unrar

--- scripts/unrar.sh
+++ scripts/unrar.sh
@@ -1,10 +1,17 @@
 #!/bin/sh -xu
 
-cd ${TR_TORRENT_DIR}/${TR_TORRENT_NAME}
+cd $TR_TORRENT_DIR
+
+if [ ! -d $TR_TORRENT_NAME ] ; then
+    exit 0
+fi
+
+cd $TR_TORRENT_NAME
 UNRAR_DIR=./unrar
 mkdir $UNRAR_DIR
 cd $UNRAR_DIR
 find .. -name *.rar -exec unrar e -o- {} \;
-find . -name *.rar -exec unrar e -o- {} \; -delete
+find . -name *.rar -exec unrar e -o- {} \;
+find . -name *.rar -exec rm {} \;
 cd ..
 rmdir -r $UNRAR_DIR
