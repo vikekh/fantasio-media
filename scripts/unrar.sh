2b7d04e051270b9a9a89e7d22625a87568bb6713
vikekh <621212+vikekh@users.noreply.github.com>
2019-10-29 10:59:54 +0000

    Wrap variables

--- scripts/unrar.sh
+++ scripts/unrar.sh
@@ -1,20 +1,20 @@
 #!/bin/sh -xu
 
-cd $TR_TORRENT_DIR
+cd "$TR_TORRENT_DIR"
 
-if [ ! -d $TR_TORRENT_NAME ] ; then
+if [ ! -d "$TR_TORRENT_NAME" ] ; then
     echo "Unable to find torrent directory, exiting early."
     exit 0
 fi
 
 echo "Begin unrar"
-cd $TR_TORRENT_NAME
+cd "$TR_TORRENT_NAME"
 UNRAR_DIR=./unrar
-mkdir $UNRAR_DIR
-cd $UNRAR_DIR
+mkdir "$UNRAR_DIR"
+cd "$UNRAR_DIR"
 find .. -name *.rar -exec unrar e -o- {} \;
 find . -name *.rar -exec unrar e -o- {} \;
 find . -name *.rar -exec rm {} \;
 cd ..
-rmdir $UNRAR_DIR
+rmdir "$UNRAR_DIR"
 echo "End unrar"
