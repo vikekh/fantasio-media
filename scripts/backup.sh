a70ecae46c139f81035710dda6a141d65078a452
vikekh <621212+vikekh@users.noreply.github.com>
2021-12-28 21:30:11 +0100

    Just archive, not gzip

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -21,13 +21,13 @@ docker stop radarr
 docker stop sonarr
 docker stop tautulli
 
-docker run --rm -v bazarr-config:/data -v $BACKUP_DIR:/backup ubuntu tar czvf /backup/bazarr-config.tar.gz /data
-docker run --rm -v jackett-config:/data -v $BACKUP_DIR:/backup ubuntu tar czvf /backup/jackett-config.tar.gz /data
-docker run --rm -v overseerr-config:/data -v $BACKUP_DIR:/backup ubuntu tar czvf /backup/overseerr-config.tar.gz /data
-docker run --rm -v plex-config:/data -v $BACKUP_DIR:/backup ubuntu tar czvf /backup/plex-config.tar.gz /data
-docker run --rm -v radarr-config:/data -v $BACKUP_DIR:/backup ubuntu tar czvf /backup/radarr-config.tar.gz /data
-docker run --rm -v sonarr-config:/data -v $BACKUP_DIR:/backup ubuntu tar czvf /backup/sonarr-config.tar.gz /data
-docker run --rm -v tautulli-config:/data -v $BACKUP_DIR:/backup ubuntu tar czvf /backup/tautulli-config.tar.gz /data
+docker run --rm -v bazarr-config:/data -v $BACKUP_DIR:/backup ubuntu tar cvf /backup/bazarr-config.tar /data
+docker run --rm -v jackett-config:/data -v $BACKUP_DIR:/backup ubuntu tar cvf /backup/jackett-config.tar /data
+docker run --rm -v overseerr-config:/data -v $BACKUP_DIR:/backup ubuntu tar cvf /backup/overseerr-config.tar /data
+docker run --rm -v plex-config:/data -v $BACKUP_DIR:/backup ubuntu tar cvf /backup/plex-config.tar /data
+docker run --rm -v radarr-config:/data -v $BACKUP_DIR:/backup ubuntu tar cvf /backup/radarr-config.tar /data
+docker run --rm -v sonarr-config:/data -v $BACKUP_DIR:/backup ubuntu tar cvf /backup/sonarr-config.tar /data
+docker run --rm -v tautulli-config:/data -v $BACKUP_DIR:/backup ubuntu tar cvf /backup/tautulli-config.tar /data
 
 docker start bazarr
 docker start jackett
