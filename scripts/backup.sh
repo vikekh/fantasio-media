2f00e032b9802621d5711c59fdbf1af1e46b50aa
vikekh <621212+vikekh@users.noreply.github.com>
2023-03-24 01:07:55 +0100

    Portainer

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -17,6 +17,7 @@ docker stop bazarr
 docker stop jackett
 docker stop overseerr
 docker stop plex
+docker stop portainer
 docker stop radarr
 docker stop sonarr
 docker stop tautulli
@@ -25,6 +26,7 @@ docker run --rm -v bazarr-config:/data -v $BACKUP_DIR:/backup ubuntu tar cvf /ba
 docker run --rm -v jackett-config:/data -v $BACKUP_DIR:/backup ubuntu tar cvf /backup/jackett-config.tar /data
 docker run --rm -v overseerr-config:/data -v $BACKUP_DIR:/backup ubuntu tar cvf /backup/overseerr-config.tar /data
 docker run --rm -v plex-config:/data -v $BACKUP_DIR:/backup ubuntu tar cvf /backup/plex-config.tar /data
+docker run --rm -v portainer-data:/data -v $BACKUP_DIR:/backup ubuntu tar cvf /backup/portainer-data.tar /data
 docker run --rm -v radarr-config:/data -v $BACKUP_DIR:/backup ubuntu tar cvf /backup/radarr-config.tar /data
 docker run --rm -v sonarr-config:/data -v $BACKUP_DIR:/backup ubuntu tar cvf /backup/sonarr-config.tar /data
 docker run --rm -v tautulli-config:/data -v $BACKUP_DIR:/backup ubuntu tar cvf /backup/tautulli-config.tar /data
@@ -33,6 +35,7 @@ docker start bazarr
 docker start jackett
 docker start overseerr
 docker start plex
+docker start portainer
 docker start radarr
 docker start sonarr
 docker start tautulli
